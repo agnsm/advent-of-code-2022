056f1d48e75a64c5bd3b5e98a1715c0de9a84140
Agnieszka <agnieszka.smolinska.99@gmail.com>
2022-12-06 11:51:41 +0100

    Add day 1

---
+++ src/solutions/day1.ts
@@ -0,0 +1,33 @@
+import { readFileSync } from 'fs';
+
+function part1(file: string) {
+  const elves = file
+    .split('\n\n')
+    .map(calories => calories.split('\n').map(val => Number(val)));
+
+  const totalCalories = elves
+    .map(calories => calories.reduce((acc, val) => acc + val))
+    .sort((a, b) => b - a);
+    
+  return totalCalories[0];
+}
+
+function part2(file: string) {
+  const elves = file
+    .split('\n\n')
+    .map(calories => calories.split('\n').map(val => Number(val)));
+
+  const totalCalories = elves
+    .map(calories => calories.reduce((acc, val) => acc + val))
+    .sort((a, b) => b - a);
+    
+  return totalCalories.slice(0, 3).reduce((acc, val) => acc + val);
+}
+
+function main() {
+  const file = readFileSync('../inputs/day1.txt', 'utf-8');
+  console.log(part1(file));
+  console.log(part2(file));
+}
+
+main();
